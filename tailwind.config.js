b932496be80f4e881cb46c3bb2e40a49c96708c7
VocHub <payomide404@gmail.com>
2023-03-25 05:15:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html",
+            "./src/script.js",
+            "./public/pages/login.html",
+            "./public/pages/signup.html"
+          ],
+  theme: {
+    colors: {
+      'mint': '#ADEFD1FF',
+      'sailorBlue': '#00203F',
+      'shadow': '#0a5dad',
+      'white': '#ffffff',
+    },
+    fontWeight: {
+      'light': 100,
+      'normal': 400,
+      'bold': 700
+    },
+    screens: {
+      sm: '200px',
+      md: '768px',
+      lg: '976px',
+      xl: '1024px',
+    },
+    extend: {
+      fontFamily: {
+        Serif: ['Epilogue', 'sans-serif'],
+        Montserrat: ['Montserrat', 'sans-serif']
+      },
+      fontSize: {
+        'small': '0.8rem',
+        'normal': '1rem',
+        'medium': '1.6rem',
+        'semiBig': '2.5rem',
+        'big': '4.5rem'
+      }
+    },
+  },
+  plugins: [],
+}
